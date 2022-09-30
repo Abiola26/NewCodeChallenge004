d5a325e056f6651443f5bff33aab2f7f0de766dc
HP <HP@BIOTECH>
2022-09-30 09:22:44 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using NewCodeChallenge004;
+
+int[] nums = { 2, 7, 11, 15 };
+int target = 13;
+
+Target hi = new Target();
+
+int[] output = hi.TwoSum(nums, target);
+
+foreach (var item in output)
+{
+    Console.WriteLine(item);
+}
---
+++ Target01.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewCodeChallenge004
+{
+    internal class Target01
+    {
+        public int[] TwoSumReview(int[] nums, int target)
+        {
+            Dictionary<int, int> numsDictionary = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int num = nums[i];
+
+                if (numsDictionary.TryGetValue(target - num, out int index))
+                {
+                    return new int[] { index, 1 };
+                }
+                numsDictionary[num] = i;
+            }
+            return null;
+
+        }
+    }
+}
